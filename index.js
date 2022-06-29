a5bf1d66e948d8f1d7b1b9f78c346eb66c13f1fa
nawaz2508 <93372202+nawaz2508@users.noreply.github.com>
2022-06-29 12:36:25 +0530

    update

--- index.js
+++ index.js
@@ -1,110 +1,108 @@
+/*
+You have been given a list of products which is having property productName, quantity and description.
 
-const listOfProducts = [{
+PROBLEM STATEMENTS:
+
+1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+{
+  "TV": 2,
+  "AC": 2,
+  "FAN": 1
+}
+
+
+
+2. you need to write a function say, getUniquePrducts which should return an array of objects by grouping the products based on the productName and summing up the quantity for the same products present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+[{
     productName: "TV",
-    quantity: 10,
+    quantity: 20,
     description: "television"
   },
   {
     productName: "AC",
-    quantity: 5,
-    description: "air conditioner"
-  },
-  {
-    productName: "TV",
     quantity: 10,
-    description: "television"
-  },
-  {
-    productName: "AC",
-    quantity: 5,
     description: "air conditioner"
   },
   {
     productName: "FAN",
     quantity: 10,
-    description: "Ceiling Fan"
+     description: "Ceiling Fan"
   }
+]
+
+*/
+
+
+
+
+const listOfProducts = [{
+  productName: "TV",
+  quantity: 10,
+  description: "television"
+},
+{
+  productName: "AC",
+  quantity: 5,
+  description: "air conditioner"
+},
+{
+  productName: "TV",
+  quantity: 10,
+  description: "television"
+},
+{
+  productName: "AC",
+  quantity: 5,
+  description: "air conditioner"
+},
+{
+  productName: "FAN",
+  quantity: 10,
+  description: "Ceiling Fan"
+}
 ];
 
-// 1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
-
-// Sample Output for the given listOfProducts will be :
-
-// {
-//   "TV": 2,
-//   "AC": 2,
-//   "FAN": 1
-// }
-
-let prodObj = {};
-let getUniqueProductCount = () => {
-    
-    for (let i = 0; i < listOfProducts.length; i++) {
-        if (!prodObj[listOfProducts[i].productName]) {
-            prodObj[listOfProducts[i].productName] = 1;
-        } else prodObj[listOfProducts[i].productName]++;
-      };
-      return JSON.stringify(prodObj);
-    }
-    document.getElementById("uniqueProduct").innerHTML=`Get Unique Product ${getUniqueProductCount()}`;
-
-console.log(JSON.stringify(prodObj));
 
 
-
-// 2. you need to write a function say, getUniquePrducts which should return an array of objects by grouping the products based on the productName and summing up the quantity for the same products present in the given list of Products considering Product Name as Key.
-
-// Sample Output for the given listOfProducts will be :
-
-// [{
-//     productName: "TV",
-//     quantity: 20,
-//     description: "television"
-//   },
-//   {
-//     productName: "AC",
-//     quantity: 10,
-//     description: "air conditioner"
-//   },
-//   {
-//     productName: "FAN",
-//     quantity: 10,
-//      description: "Ceiling Fan"
-//   }
-// ];
-
+let uniqueProduct = () => {
+  let prodObj= {};
+  for (let i = 0 ; i < listOfProducts.length ; i++) {
+      if ( !prodObj[listOfProducts[i].productName]) {
+          prodObj[listOfProducts[i].productName] = 1;
+      } else prodObj[listOfProducts[i].productName]+=1;
+    };
+    return prodObj;
+  }
+  
+console.log(uniqueProduct());
+  
 let list=[];
-let getUniquePrducts =()=>{
-    
-    for (let i = 0; i < listOfProducts.length; i++) {
-    let flag=true;
-            for(let j=0;j<list.length;j++){
-            
-                    if(listOfProducts[i].productName==list[j].productName){
-                        list[j].quantity+=listOfProducts[i].quantity;
-                        flag=false;
-                       }
-                    }
-                    if(flag===true){
-                    list.push(listOfProducts[i]);
-                    }
-        }
-        return JSON.stringify(list);
-        
-        };
-
-        document.getElementById("uniqueQnty").innerHTML=`${getUniquePrducts()}`;
-        
-        // console.log(list);
+let getUniqueProducts =()=>{
+  
+  for (let i = 0; i < listOfProducts.length; i++) {
+  let flag=true;
+          for(let j=0;j<list.length;j++){
+          
+                  if(listOfProducts[i].productName==list[j].productName){
+                      list[j].quantity+=listOfProducts[i].quantity;
+                      flag=false;
+                     }
+                  }
+                  if(flag===true){
+                  list.push(listOfProducts[i]);
+                  }
+      }
+      return list;
       
-    
-
-    
-    
-
-console.log(JSON.stringify(list));
-
-
-
+      };
+  
+      
+console.log(getUniqueProducts());
 
 
